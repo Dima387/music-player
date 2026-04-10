ae956544e6471ad52d52ad8e579ab8100633fd8e
Dmytro Kolesnikov <kdmitro466@gmail.com>
2026-04-10 19:54:22 +0300

    feat:Створено масив саундтреків

---
+++ src/data/traks.js
@@ -0,0 +1,53 @@
+const tracks = [
+  {
+    id: 1,
+    name: "Im A Feeder",
+    artist: "Yellow Fang",
+    album: "The Greatest",
+    cover: "yellow-fang.jpg",
+    src:"Im-a-feeder.mp3"
+  },
+    {
+    id: 2,
+    name: "Ой Марічко, чичері",
+    artist: "Володимир Іжицький",
+    album: "Ой Марічко, чичері",
+    cover: "oi-marichko.jpg",
+    src:"Oi-marichko.mp3"
+  },
+  {
+    id: 3,
+    name: "Let's Try",
+    artist: "Khaotung Thanawat",
+    album: "เอาเลยมั้ย (เพลงประกอบซีรีส์ 'Only Friends เพื่อนต้องห้าม')",
+    cover: "lets-try.jpg",
+    src:"Lets-try.mp3"
+  },
+  {
+    id: 4,
+    name: "Вишнi",
+    artist: "Wellboy",
+    album: "Вишнi",
+    cover: "vyshni.jpg",
+    src:"Vyshni.mp3"
+  },
+  {
+    id: 5,
+    name: "Risk It All",
+    artist: "Bruno Mars",
+    album: "The Romantic",
+    cover: "risk-it-all.jpg",
+    src:"Risk-it-all.mp3"
+  },
+  {
+    id: 6,
+    name: "NSHALLAH",
+    artist: "ZUNA",
+    album: "NSHALLAH",
+    cover: "nshallah.jpg",
+    src:"Nshallah.mp3"
+  }
+
+]
+
+export default tracks;
