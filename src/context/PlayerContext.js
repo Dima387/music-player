468163fa1d57fcd6fae3af60b291b95d11e6c68c
Dmytro Kolesnikov <kdmitro466@gmail.com>
2026-04-11 10:00:55 +0300

    feat: Додано пусту логіку (заглушку)

---
+++ src/context/PlayerContext.js
@@ -0,0 +1,14 @@
+const initialState = {
+  currentTrack: null,
+  isPlaying: false,
+  volume: 1,
+  favorites: [],
+};
+
+const playTrack = () => {};
+const pauseTrack = () => {};
+const nextTrack = () => {};
+const prevTrack = () => {};
+const setVolume = () => {};
+const addToFavorites = () => {};
+const removeFromFavorites = () => {};
