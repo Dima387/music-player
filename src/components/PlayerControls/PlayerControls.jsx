2c23a7489e316db99b8ecea17fbfc1bebe8063bc
fiuari <fiuari.federico@gmail.com>
2026-04-11 10:25:10 +0300

    feat: створено верстку компонентів програвача

---
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -0,0 +1,11 @@
+export default function PlayerControls() {
+    return (
+        <div className="player-controls">
+            <button className="mixbtn"> ⇄ </button>
+            <button className="previousbtn"> ⏮ </button>
+            <button className="playbtn"> ▶ </button>
+            <button className="nextbtn"> ⏭ </button>
+            <button className="repeatbtn"> ↻ </button>
+        </div>
+    )
+}
