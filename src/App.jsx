b38741ce996aa6e6a715a328c736eb8ca9df72ef
Dmytro Kolesnikov <kdmitro466@gmail.com>
2026-04-11 10:57:25 +0300

    feat:Виправлено помилки

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-// import Home from "./pages/Home/Home";
-// import Search from "./pages/Search/Search";
-// import Favourites from "./pages/Favourites/Favourites";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar/Navbar";
-// import PlayerBar from "./components/PlayerBar/PlayerBar";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
+import Favourites from "./pages/Favourites/Favourites";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import PlayerBar from "./components/PlayerBar/PlayerBar";
 
 function App() {
 
