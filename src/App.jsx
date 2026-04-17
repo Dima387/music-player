4e2950e664f1f0c5d3002bc24c0ad067d8e7dfea
fiuari <fiuari.federico@gmail.com>
2026-04-17 18:58:11 +0300

    feat: додано стилі до карток треків та сторінки Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Home from "./pages/Home/Home";
 import Search from "./pages/Search/Search";
 import Favourites from "./pages/Favourites/Favourites";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 // import PlayerBar from "./components/PlayerBar/PlayerBar";
 
@@ -10,7 +10,6 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
       <Navbar />
 
       <Routes>
@@ -20,7 +19,6 @@ function App() {
       </Routes>
 
       {/* <PlayerBar /> */}
-    </BrowserRouter> 
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import TrackCard from "../../components/TrackCard/TrackCard";
 import tracks from "../../data/tracks";
+import './home.css';
 
 export default function Home() {
     return (
         <div className="home-page">
             <h1>Welcome back!</h1>
-            {tracks.map((track, index) => <TrackCard key={index} title={track.title} artist={track.artist} cover={track.cover}/>)}
+            <div className="tracks">
+                {tracks.map((track, index) => <TrackCard key={index} name={track.name} artist={track.artist} cover={track.cover}/>)}
+            </div>
         </div>
     )
 }
