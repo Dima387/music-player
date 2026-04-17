e3d62f215de8458f6633c481a4d1825187f28d07
Dmytro Kolesnikov <kdmitro466@gmail.com>
2026-04-17 18:38:04 +0300

    feat:Видалено помилку

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
       <Navbar />
 
       <Routes>
@@ -20,7 +19,6 @@ function App() {
       </Routes>
 
       {/* <PlayerBar /> */}
-    </BrowserRouter> 
     </>
   )
 }
